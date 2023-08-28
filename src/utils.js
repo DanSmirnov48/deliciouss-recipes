557fd49686f119bd67d95efd361c2a0fe456d562
DanSmirnov48 <daniel.smirnov17@gmail.com>
2023-08-28 23:24:06 +0100

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,6 @@ export const JSON_API = 'http://localhost:8000/api'
 //export const JSON_API = 'https://deliciouss-recipes.herokuapp.com/api'
 
 // API KEY one
-// export const SPOONACULAR_API_KEY = 'deb18101437e43d78803e73825ccfbac'
-// API KEY two
-export const SPOONACULAR_API_KEY = 'c92d1b3a396d441287e68e044aa6fc12'
+export const SPOONACULAR_API_KEY = '######################'
 // Calories API KEY
-export const RAPID_API_KEY = '9b67820f38msh82194c8722ad6aep1c5abajsncbab14d0f928'
+export const RAPID_API_KEY = '#############################'
